62ccfad576fe8b704319b7a422e8a53698b290a6
IDerkBot <iderkbot@gmail.com>
2024-07-17 12:26:07 +0300

    update

--- TestMech/ViewModels/MainWindowViewModel.cs
+++ TestMech/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using ShMeco.Infrastructure.ViewModel;
+﻿using System.Windows.Input;
+using ShMeco.Infrastructure.ViewModel;
+using TestMech.Infrastructure.Commands;
 
 namespace TestMech.ViewModels;
 
@@ -6,7 +8,7 @@ public class MainWindowViewModel : BaseViewModel
 {
     #region Properties
 
-    #region AxisY1 : AxisControlViewModel - 
+    #region AxisY1 : AxisControlViewModel -
 
     private AxisControlViewModel _axisY1;
 
@@ -19,7 +21,7 @@ public class MainWindowViewModel : BaseViewModel
 
     #endregion AxisY1
 
-    #region AxisY2 : AxisControlViewModel - 
+    #region AxisY2 : AxisControlViewModel -
 
     private AxisControlViewModel _axisY2;
 
@@ -32,7 +34,7 @@ public class MainWindowViewModel : BaseViewModel
 
     #endregion AxisY2
 
-    #region AxisX1 : AxisControlViewModel - 
+    #region AxisX1 : AxisControlViewModel -
 
     private AxisControlViewModel _axisX1;
 
@@ -45,7 +47,7 @@ public class MainWindowViewModel : BaseViewModel
 
     #endregion AxisX1
 
-    #region AxisU1 : AxisControlViewModel - 
+    #region AxisU1 : AxisControlViewModel -
 
     private AxisControlViewModel _axisU1;
 
@@ -58,7 +60,7 @@ public class MainWindowViewModel : BaseViewModel
 
     #endregion AxisU1
 
-    #region AxisU2 : AxisControlViewModel - 
+    #region AxisU2 : AxisControlViewModel -
 
     private AxisControlViewModel _axisU2;
 
@@ -71,7 +73,7 @@ public class MainWindowViewModel : BaseViewModel
 
     #endregion AxisU2
 
-    #region AxisV1 : AxisControlViewModel - 
+    #region AxisV1 : AxisControlViewModel -
 
     private AxisControlViewModel _axisV1;
 
@@ -84,7 +86,7 @@ public class MainWindowViewModel : BaseViewModel
 
     #endregion AxisV1
 
-    #region AxisZ1 : AxisControlViewModel - 
+    #region AxisZ1 : AxisControlViewModel -
 
     private AxisControlViewModel _axisZ1;
 
@@ -97,7 +99,7 @@ public class MainWindowViewModel : BaseViewModel
 
     #endregion AxisZ1
 
-    #region AxisZ2 : AxisControlViewModel - 
+    #region AxisZ2 : AxisControlViewModel -
 
     private AxisControlViewModel _axisZ2;
 
@@ -110,41 +112,80 @@ public class MainWindowViewModel : BaseViewModel
 
     #endregion AxisZ2
 
+    #region Speed : int -
+
+    private int _speed;
+
+    /// <summary>  </summary>
+    public int Speed
+    {
+        get => _speed;
+        set => SetField(ref _speed, value);
+    }
+
+    #endregion AxisZ2
+
     #endregion Properties
 
     public static App App;
-    
-    public MainWindowViewModel()
-    {
-        AxisY1 = new AxisControlViewModel("Y1", readActual: 0, setTarget: 0, move: 11) { Maximum = 1000, Minimum = 0 };
-        AxisY2 = new AxisControlViewModel("Y2", readActual: 1, setTarget: 1, move: 11) { Maximum = 1000, Minimum = 0 };
-        AxisU1 = new AxisControlViewModel("U1", readActual: 2, setTarget: 2, move: 11) { Maximum = 1000, Minimum = 0 };
-        AxisU2 = new AxisControlViewModel("U2", readActual: 3, setTarget: 3, move: 11) { Maximum = 1000, Minimum = 0 };
-        AxisZ1 = new AxisControlViewModel("Z1", readActual: 4, setTarget: 4, move: 11) { Maximum = 1000, Minimum = 0 };
-        AxisZ2 = new AxisControlViewModel("Z2", readActual: 5, setTarget: 5, move: 11) { Maximum = 1000, Minimum = 0 };
-        AxisX1 = new AxisControlViewModel("X", readActual: 6, setTarget: 6, move: 11) { Maximum = 1000, Minimum = 0 };
-        AxisV1 = new AxisControlViewModel("V", readActual: 7, setTarget: 7, move: 11) { Maximum = 1000, Minimum = 0 };
 
-        Thread _update = new Thread(Update);
-        _update.Start();
+    #region ChangeSpeed - Переход домой
+
+    ///<summary> Переход домой </summary>
+    public ICommand ChangeSpeedCommand { get; }
+
+    private void OnChangeSpeedCommandExecuted(object parameter)
+    {
+        if (int.TryParse((string)parameter, out var value))
+        {
+            SetSpeed(value);
+        }
     }
 
-    private short[] actualPosition = new short[2];
-
-    private void Update()
+    private bool CanChangeSpeedCommandExecute(object parameter)
     {
+        return true;
+    }
 
-        //while (true)
-        //{
-        //    if (App.Modbus == null) return;
+    #endregion
+    
+    #region ChangeSpeed - Переход домой
 
-        //    actualPosition = new short[2];
+    ///<summary> Переход домой </summary>
+    public ICommand StopMotorsCommand { get; }
 
-        //    App.Modbus.ReadInputRegister(12, 1, actualPosition);
-        //    //double value = (actualPosition[0] << 16 | actualPosition[1]) * FirstParam / SecondParam;
+    private void OnStopMotorsCommandExecuted(object parameter)
+    {
+        App.Modbus.WriteSingleRegister(12, 2);
+    }
 
-        //    //ActualPosition = actualPosition[0];
-        //}
+    private bool CanStopMotorsCommandExecute(object parameter)
+    {
+        return true;
+    }
 
+    #endregion
+
+    public MainWindowViewModel()
+    {
+        ChangeSpeedCommand = new RelayCommand(OnChangeSpeedCommandExecuted, CanChangeSpeedCommandExecute);
+        StopMotorsCommand = new RelayCommand(OnStopMotorsCommandExecuted, CanStopMotorsCommandExecute);
+        
+        AxisY1 = new AxisControlViewModel("Y1", readActual: 0, setTarget: 0, move: 12) { Maximum = 1000, Minimum = 0 };
+        AxisY2 = new AxisControlViewModel("Y2", readActual: 1, setTarget: 1, move: 12) { Maximum = 1000, Minimum = 0 };
+        AxisU1 = new AxisControlViewModel("U1", readActual: 2, setTarget: 2, move: 12) { Maximum = 1000, Minimum = 0 };
+        AxisU2 = new AxisControlViewModel("U2", readActual: 3, setTarget: 3, move: 12) { Maximum = 1000, Minimum = 0 };
+        AxisZ1 = new AxisControlViewModel("Z1", readActual: 4, setTarget: 4, move: 12) { Maximum = 1000, Minimum = 0 };
+        AxisZ2 = new AxisControlViewModel("Z2", readActual: 5, setTarget: 5, move: 12) { Maximum = 1000, Minimum = 0 };
+        AxisX1 = new AxisControlViewModel("X", readActual: 6, setTarget: 6, move: 12) { Maximum = 1000, Minimum = 0 };
+        AxisV1 = new AxisControlViewModel("V", readActual: 7, setTarget: 7, move: 12) { Maximum = 1000, Minimum = 0 };
+
+        Speed = 100;
+        SetSpeed(100);
+    }
+    
+    private void SetSpeed(int value)
+    {
+        App.Modbus.WriteSingleRegister(11, (ushort)value);
     }
 }
